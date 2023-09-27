bee17e68e851d83ee3bf5207a7eec0216da2b103
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-09-27 19:51:21 +0200

    remove first floor modulo

--- reverse.js
+++ reverse.js
@@ -59,16 +59,31 @@ function reverseV2(n) {
 //console.log(Math.floor((1234 % 10) / 1));
 
 function reverseV3(n) {
-  let reversed = 0
+  let reversed = 0;
   while (n > 0) {
-    lastDigit = Math.floor(n % 10);
-    reversed = reversed * 10 + lastDigit
+    // Is Math floor even needed here? Let's test
+    // I guess not, maybe for floats only
+    //lastDigit = Math.floor(n % 10);
+
+    // So here we get the modulo, so 1234 % 10 will return 4 which is the last digit
+    lastDigit = n % 10;
+    // On the first run this will be 0 * 10 + 4
+    // reversed is then 4
+    // On the second run it will be 4 * 10 = 40 + 3 + 43
+    // These are the last 2 digits
+    // On the third run it will be 43 * 10 = 430 + 2 = 432
+    // 432 * 10 = 4320 + 1 = 4321
+    reversed = reversed * 10 + lastDigit;
+
+    // Math floor is also probably not needed here, only for floats
+    // Turns our it is needed as otherwise the loop never ends since we never reach a negative number
     n = Math.floor(n / 10);
+    //n = n / 10;
   }
-  return reversed
+  return reversed;
 }
 
-console.log(reverseV3(number))
+console.log(reverseV3(number));
 //console.log(reverseV2(number))
 
 //console.log(reverse(number));
